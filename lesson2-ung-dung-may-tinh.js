c92169f38c1c8fffc00c2174d00719e7a87c6280
nham <nhamnd.hmu@gmail.com>
2019-10-14 16:24:24 +0700

    the module1

---
+++ lesson2-ung-dung-may-tinh.js
@@ -0,0 +1,36 @@
+function tong() {
+    let arg1 = +document.getElementById("calculator-arg_1").value;
+    let arg2 = +document.getElementById("calculator-arg_2").value;
+    let calculateResult = arg1 + arg2;
+    document.getElementById("calculated").value = calculateResult;
+}
+
+function hieu() {
+    let arg1 = +document.getElementById("calculator-arg_1").value;
+    let arg2 = +document.getElementById("calculator-arg_2").value;
+    let calculateResult = arg1 - arg2;
+    document.getElementById("calculated").value = calculateResult;
+}
+
+function tich() {
+    let arg1 = +document.getElementById("calculator-arg_1").value;
+    let arg2 = +document.getElementById("calculator-arg_2").value;
+    let calculateResult = arg1 * arg2;
+    document.getElementById("calculated").value = calculateResult;
+}
+
+function thuong() {
+    let arg1 = +document.getElementById("calculator-arg_1").value;
+    let arg2 = +document.getElementById("calculator-arg_2").value;
+    let calculateResult = arg1 / arg2;
+    document.getElementById("calculated").value = calculateResult;
+
+}
+function reuseCalculatedValue() {
+    let calculatedValue = document.getElementById("calculated").value;
+    document.getElementById("calculator-arg_1").value = calculatedValue;
+    document.getElementById("calculator-arg_2").value = "";
+    document.getElementById("calculated").value = "";
+    document.getElementById("calculator-arg_2").focus();
+}
+
--- money.js
+++
@@ -1,9 +0,0 @@
-function converted() {
-    let sotien = +document.getElementById("amount").value;
-    let so1 = +document.getElementById("from").value;
-    let so2 = +document.getElementById("to").value;
-
-    let calculateResult =  so1 / so2 * sotien;
-
-    document.getElementById("calculated").value = calculateResult;
-}
